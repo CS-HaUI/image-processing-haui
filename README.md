7c9076719763cf427973335ddb91700f3a652fe1
nguyenvanvutlv <nguyenvanvu.tlv@gmail.com>
2023-05-15 22:51:04 +0700

    version 1.1 [15/5/2023] Update description Vietnamese

--- README.md
+++ README.md
@@ -12,3 +12,142 @@ Bài tập lớn môn xử lý ảnh số và thị giác máy tính
 | Ngô Thế Tài           | [thetai](https://github.com/ngothetai)             |
 | Đỗ Trung Hiếu         | [HieuDNS](https://github.com/HieuDNS)        |
 | Trần Thị Khánh Linh   | [linhlukar](https://github.com/linhlukar)          |
+
+Cấu trúc thư mục
+
+- Cần cài đặt thư mục data ở máy local
+
+- Và các file ảnh được đặt tên như mô tả bên dưới
+    
+    bắt đầu từ 1 -> N
+
+<pre>
+📦image-processing-haui
+ ┣ 📂data
+ ┃ ┣ 📂json
+ ┃ ┃ ┣ 📜data.json
+ ┃ ┗ 📂raw
+ ┃ ┃ ┣ 📜1.jpg
+ ┃ ┃ ┣ 📜....
+ ┃ ┃ ┗ 📜N.jpg
+ ┣ 📂process
+ ┃ ┗ 📜function.py
+ ┣ 📂visuallization
+ ┃ ┣ 📂demo
+ ┃ ┃ ┗ 📜result.png
+ ┃ ┣ 📜plotly_custom.py
+ ┃ ┗ 📜subplots.py
+ ┣ 📜.gitignore
+ ┣ 📜LICENSE
+ ┣ 📜main.py
+ ┣ 📜README.md
+ ┣ 📜requirements.txt
+ ┗ 📜requirements.yml
+</pre>
+
+# Cách sử dụng
+
+## Cài đặt môi trường
+
+- Hãy chắc chắc chắn để cấu trúc như ở phần mô tả trên
+
+- Cài đặt sẵn [Anaconda](https://www.anaconda.com/download), [gitbash](https://git-scm.com/downloads)
+
+- Mở anaconda
+
+``` bash
+# clone repo về máy
+git clone https://github.com/CS-HaUI/image-processing-haui.git
+cd image-processing-haui
+
+# chuyển sang nhánh 1-phase-1
+git checkout 1-phase-1
+
+# cài đặt môi trường có thể rất lâu khi chạy trên window
+# conda env create -n ENVNAME --file ENV.yml
+conda env create -n image --file requirements.yml
+```
+
+## Cách sử dụng 
+
+
+``` bash
+# di chuyển đến thư mục repo
+cd image-processing-haui
+
+# activate môi trường
+conda activate image
+```
+
+### Đọc dữ liệu, là load dữ liệu
+
+- file main.py
+
+```python
+# load dữ liệu từ file raw, chọn kích cỡ là (512, 512), đuôi file chọn tất cả "*"
+from process.function import LBP
+objects = LBP(path= "data/raw/", size= (512, 512))
+
+"""
+output:
+LOADING FILE!!!
+FILE: *
+Read file  .*: 100%|████████████████████████████████████████████████| 32/32 [00:00<00:00, 42.14it/s]
+Extract Feature: 32it [00:00, 236.76it/s]
+"""
+```
+
+```python
+# load dữ liệu từ file raw, chọn kích cỡ là (512, 512), đuôi file chọn tất cả "*", là lưu lại file json
+from process.function import LBP
+objects = LBP(path= "data/raw/", size= (512, 512))
+objects.save(path_file= "data/json/data.json")
+
+"""
+output:
+LOADING FILE!!!
+FILE: *
+Read file  .*: 100%|████████████████████████████████████████████████| 32/32 [00:00<00:00, 42.46it/s]
+Extract Feature: 32it [00:00, 339.85it/s]
+Save file json: 32it [00:00, 13339.07it/s]
+"""
+```
+
+
+```python
+# load dữ liệu từ file JSON
+from process.function import LBP
+objects = LBP(from_path= True, load_path= "data/json/data.json")
+"""
+output:
+Read data from json: 32it [00:00, 31025.83it/s]
+"""
+```
+
+### Lấy thông tin ảnh
+```python
+from process.function import LBP
+objects = LBP(from_path= True, load_path= "data/json/data.json")
+
+# lấy ảnh RGB từ đường dẫn
+rgb_path = objects.get_image_color_from_path(path= ["data/raw/1.jpg", "data/raw/10.jpg"])
+# (2, 512, 512, 3) 2 ảnh có shape là (512, 512, 3)
+# nếu muốn lấy từng ảnh thì chỉ cần rgb_path[1]
+
+
+# lấy ảnh RGB từ chỉ số
+rgb_path = objects.get_image_color_from_id(ids= [1, 12])
+# (2, 512, 512, 3) 2 ảnh có shape là (512, 512, 3)
+# nếu muốn lấy từng ảnh thì chỉ cần rgb_path[1]
+
+# lấy ảnh LBP từ đường dẫn
+lbp_path = objects.get_image_lbp_from_path(path= ["data/raw/1.jpg", "data/raw/10.jpg"])
+# (2, 512, 512, 3) 2 ảnh có shape là (512, 512, 3)
+# nếu muốn lấy từng ảnh thì chỉ cần rgb_path[1]
+
+
+# lấy ảnh RGB từ chỉ số
+lbp_path = objects.get_image_lbp_from_id(ids= [1, 12])
+# (2, 512, 512, 3) 2 ảnh có shape là (512, 512, 3)
+# nếu muốn lấy từng ảnh thì chỉ cần rgb_path[1]
+```
--- process/function.py
+++ process/function.py
@@ -9,6 +9,19 @@ from tqdm import tqdm
 
 class BaseImage:
     """
+        Lớp Cơ Sở
+        các tham số
+            path : đường dẫn đến folder chứa ảnh 
+                    trong dự án này là path = "data/raw/"
+                    
+            types: đuôi file ảnh
+                    nếu types= "*" lớp BaseImage sẽ đọc tất cả các ảnh trong folder
+                    hoặc nếu không muốn đọc tất cả thì truyền vào 1 list đuôi file
+                                                                    ["png", "jpg", ...]
+            size : kích cỡ của ảnh mặc định là (128, 128), tất cả các file sẽ được sử lý bằng với kích cỡ này
+            
+        NOTE: ảnh được đọc vào là ảnh BGR    
+        
         Args:
             path :   path to folder image
             types:   end of file, "*" or ['jpg', 'png']
@@ -18,76 +31,120 @@ class BaseImage:
                  path, 
                  types = "*", 
                  size=(128, 128)):
-        
+        # đường dẫn ảnh
         self.__path = path
+        # đuôi file
         self.__types = types
+        # kích cỡ ảnh
         self.__size = size
+        # đọc ảnh và đường dẫn của mỗi ảnh
         self.__image, self.__pathImage = self.__getImageFromPath()
         # (N, 128, 128, 3)
         
     # Read image from self.__path
     def __getImageFromPath(self):
-        """_summary_
-        Args:
-            None
-        Returns:
-            __image (nparray): list image
-            __path  (nparray): list path for each image
-        """
         print("LOADING FILE!!!")
         __image = []
         __path  = []
-        for type_file in tqdm(iterable= self.__types):
+        # duyệt qua tất cả các kiểu đuôi file : "*" hoặc ["png", "jpg", ..., ]
+        for type_file in self.__types:
             print(f"FILE: {type_file}")
             for file in tqdm(glob(self.__path + type_file), desc= f"Read file  .{type_file}"):
-                __image.append( cv2.resize(cv2.imread(file), self.__size))
+                # đường dẫn file ảnh
+                
+                # đọc ảnh bằng opencv, chuyển lại kích cơ về self.__size ví dụ: (128, 128) hoặc (512, 512)...
+                __image.append( cv2.resize(cv2.imread(file), self.__size) )
+                
+                # lưu lại đường dẫn file ảnh
                 __path.append( file )
+                
+        # chuyển về dạng numpy array
         __image = np.asarray(__image)
         __path = np.asarray(__path)
+        
         return __image, __path
 
-    
+    # đặt lại file ảnh gốc
     def setImage(self, image):
         self.__image = image
 
-    
+    # lấy file ảnh gốc
     def getImage(self):
         return self.__image.copy()
     
-    
+    # lấy kích cỡ ảnh
     def getSize(self):
         return self.__size
     
-    
+    # đặt lại kích cỡ ảnh
     def setSize(self, size):
         self.__size = size
         
-        
+    # lấy đường đẫn ảnh root
+    # vd: "data/raw/"
     def getPath(self):
         return self.__path
     
-    
+    # đặt đường dẫn ảnh root
+    # vd: "data/raw/" -> "data/image/"
     def setPath(self, path):
         self.__path = path
 
-
+    # lấy đường dẫn của từng file ảnh
+    """
+        ví dụ: 
+            ["data/raw/1.jpg", "data/raw/2.jpg", ..., "data/raw/N.jpg"]
+    """
     def getPathImage(self):
         return self.__pathImage
     
-    
+    # đặt đường dẫn của từng file ảnh
+    """
+    NOTE: chú ý khi đặt lại các tên file vẫn phải được đặt theo thứ từ 1.jpg->N.jpg
+        ví dụ:
+            ["data/raw/1.jpg", "data/raw/2.jpg", ..., "data/raw/N.jpg"]
+            -->
+            ["data/image/1.jpg", "data/image/2.jpg", ..., "data/image/N.jpg"]
+    """
     def setPathImage(self, path):
         self.__pathImage = path
         
 
 class LBP(BaseImage):
     """_summary_
-        Args:
-            path     :   path to folder image
-            types    :   end of file, "*" or ['jpg', 'png']
-            size     :   size of image, default = (128, 128)
-            from_path:   Boolean
-                         If True, data read from file json else data read from path
-            load_path:   path to file json
+    
+        Lớp LBP kế thừa lớp BaseImage sẽ kế thừa các hàm, biến không phải là hàm, biến đặc biệt
+                                                                        hàm, biến đặc biệt là có __ ở đầu
+                                                                        ví dụ: self.__path
+                                                                               def __load()
+                                                                        Nếu muốn truy cập thì phải thông qua
+                                                                        hàm get, set của lớp cha
+                                                                        ví dụ:
+                                                                                def get()
+                                                                                def set()
+        BaseImage là lớp cha, LBP là lớp con
+        
+        NOTE:
+            Có 2 cách lựa chọn của lớp LBP là đọc từ file raw ví dụ: "data/raw/"
+                                           hoặc đọc từ file json ví dụ: "data/json/data.json"
+            
+            Mô tả file json:
+                {
+                    "size_image" : [128, 128],
+                    "0" : {
+                        "vector_feature" : "Đặc điểm của ảnh",
+                        "path"           : "đường dẫn ảnh"
+                    },
+                    "1" : {
+                        ....
+                    }
+                }
+            self.__imageColor = None nếu đọc file từ json ngược lại bằng 1 danh sách các ảnh (N, 128, 128, 3) nếu   
+                                                                                    đọc từ file raw
+            self.__imageLBP   = None nếu đọc file từ json ngược lại bằng 1 danh sách các ảnh (N, 128, 128, 3) được
+                                                                                    chuyển từ self.__imageColor
+            
+            Nếu đọc dữ liệu từ file json thì self.__imageColor và self.__imageLBP là None
     """
     def __init__(self, path = "", 
                        types = "*",
@@ -96,84 +153,95 @@ class LBP(BaseImage):
                        load_path = ""):
         self.__imageColor = None
         self.__imageLBP   = None
+        
+
         if from_path:
+            # đọc dữ liệu từ file json
+            # vector đặc trưng
+            # đường dẫn từng file ảnh
+            # kích cỡ file ảnh
             self.__vector_feature, self.path, self.size = self.__load(load_path)
+            # đặt lại kích cỡ ảnh cho lớp Cha
             self.setSize(self.size)
+            # đặt lại đường dẫn cho mỗi file ảnh cho lớp Cha
             self.setPathImage(self.path)
         else:
+            # đọc dữ liệu từ file raw
             super().__init__(path, types, size)
             self.__imageColor = self.getImage()
             # convert BGR -> HSV -> LBP
             self.__imageLBP   = self.__change2LBP()
+            # trích xuất đặc trưng từ file raw
             self.__vector_feature = self.__extract_feature()
             
         
     def __change2LBP(self):
         """_summary_
-        Args:
-            None
-
-        Returns:
-            __lbp (nparray): shape=(N, 128, 128 3) [LBP image]
-                             convert rgb, bgr to lbp
+        NOTE:
+        [version 1.0] Hiện đang dùng
+            Chuyển ảnh từ BGR sang GRAYSCALE 
+            chuyển GRAYSCALE sang LBP
+            
+            
+        [version 1.1] Dự kiến
+            Chuyển ảnh từ BGR sang HSV 
+            chuyển HSV sang LBP
         """
-        # start from (0, 0) --> (0, 1) --> (0, 2)
-        #            --> (1, 2) --> (2, 2) --> (2, 1)
-        #            --> (2, 0) --> (1, 0)
         __lbp = []
         for index, image in enumerate(self.__imageColor):
             temp = image.copy()
-            # convert BGR -> HSV
+            # convert BGR -> GRAY
             r, c, channels = temp.shape
-            temp = cv2.cvtColor(temp, cv2.COLOR_BGR2HSV)
+            temp = cv2.cvtColor(temp, cv2.COLOR_BGR2GRAY)
             
-            # convert HSV -> LBP
-            for channel in range(channels):
-                temp[:, :, channel] = local_binary_pattern(temp[:, :, channel], 8, 1, 'nri_uniform')
+            # convert GRAY -> LBP
+            temp = local_binary_pattern(temp, 8, 1, 'nri_uniform')
             __lbp.append(temp)
+            
         return np.asarray(__lbp)
             
                 
     
     def __extract_feature(self):
         """_summary_
-        Args:
-            None
-        Returns:
-            __histogram (nparray): vector feature from image -> (N, 177)
+        [version 1.0] : Trích xuất đặc trưng từ ảnh LBP sang HISTOGRAM 
+                        được gọi khi đọc từ file raw
+        [version 1.1] : Cập nhật sau
         """
         __histogram = []
-        for index, value in enumerate(self.__imageLBP):
+        for index, value in tqdm(enumerate(self.__imageLBP), desc="Extract Feature"):
             image = value.copy()
-            r, c, channels = image.shape
-            hist_array = np.asarray([])
-            for channel in range(channels):
-                max_bins = np.max(image[:, :, channel]) + 1
-                hist, _ = np.histogram(image[:, :, channel], 
-                                       bins= max_bins, 
-                                       range=(0, max_bins))
-                hist_array = np.concatenate((hist_array, hist))
-            __histogram.append(hist_array)
+            r, c = image.shape
+                
+            # tính toán histogram từ lbp
+            max_bins = int(np.max(image) + 1)
+            hist, _ = np.histogram(image, 
+                                bins= max_bins, 
+                                range=(0, max_bins))              
+            __histogram.append(hist)
+            
         return np.asarray(__histogram)
     
     
     def save(self, path_file):
         """_summary_
-        Args:
-            path_file (str): path file json to save data
-            
-        Returns:
-            None
+        lưu đặc trưng của ảnh vào file json
+        các đặc trưng được lưu như sau:
+            "size_image"     : Kích cỡ của tất cả các ảnh
+            "vector_feature" : Đặc trưng của mỗi ảnh
+            "path"           : Đường dẫn của mỗi ảnh
         """
         _dict = dict()
         _dict["size_image"] = self.getSize()
+        __path_image = self.getPathImage()
         for index, value in tqdm(enumerate(self.__vector_feature), desc= "Save file json"):
             vector_feature = " ".join(value.astype(str))
             rows, cols = self.getSize()
             size_image = f"{rows} {cols}"
+            end_file = __path_image[index].split('.')[-1]
             dict_image = {
                 "vector_feature" : vector_feature,
-                "path"           : f"{self.getPath()}{index+1}.jpg"
+                "path"           : f"{self.getPath()}{index+1}.{end_file}"
             }
             _dict[f"{index+1}"] = dict_image
         with open(path_file, "w") as outfile:
@@ -182,14 +250,7 @@ class LBP(BaseImage):
     
     def __load(self, path_file):
         """_summary_
-
-        Args:
-            path_file (str): path file json to load data
-            
-        Returns:
-            vector_feature (nparray)
-            path           (nparray)
-            size_image     (tuple)
+        Đọc dữ liệu từ file json khiw đã được lưu
         """
         data = json.load(open(path_file))
         size_image = tuple(data['size_image'])
@@ -221,15 +282,12 @@ class LBP(BaseImage):
     
     def get_image_color_from_path(self, path: list):
         """_summary_
-
-        Args:
-            path (list): list path to folder image
-
-        Returns:
-            result (nparray): list image
+        Lấy ra các ảnh dạng RGB bằng một danh sách các dường dẫn
+        Ví dụ: path = ["data/raw/1.jpg", "data/raw/2.jpg", ...]
         """
         if self.__imageColor is not None:
             # situation load from folder
+            # Trường hợp đọc từ file raw
             __path = self.getPathImage()
             result = []
             for file in path:
@@ -240,6 +298,7 @@ class LBP(BaseImage):
             return np.asarray(result)
         else:
             # situation load from JSON
+            # Trường hợp đọc từ file JSON
             result = []
             for file in path:
                 result.append( cv2.resize(
@@ -250,15 +309,13 @@ class LBP(BaseImage):
         
     def get_image_color_from_id(self, ids: list):
         """_summary_
-
-        Args:
-            id (list): list id to image
-
-        Returns:
-            result (nparray): list image
+        Lấy ra các ảnh dạng RGB bằng một danh sách các chỉ số ảnh 
+        NOTE: các chỉ số bắt đầu từ 1 cho đến N : số lượng ảnh có trong cơ sở dữ liệu
+        Ví dụ: ids = [1, 2, ..., N]
         """
         if self.__imageColor is not None:
             # situation load from folder
+            # Trường hợp đọc từ file raw
             result = []
             for id in ids:
                 assert 1 <= id <= self.__imageColor.shape[0]
@@ -267,6 +324,7 @@ class LBP(BaseImage):
             return np.asarray(result)
         else:
             # situation load from JSON
+            # Trường hợp đọc từ file JSON
             list_path = self.getPathImage()
             result = []
             for id in ids:
@@ -278,16 +336,8 @@ class LBP(BaseImage):
                 result.append(image)
             return np.asarray(result)
       
-
+    # giống với hàm get_image_color_from_path ở trên nhưng được đọc ở dạng lbp
     def get_image_lbp_from_path(self, path: list):
-        """_summary_
-
-        Args:
-            path (list): list path to folder image
-
-        Returns:
-            result (nparray): list image
-        """
         if self.__imageLBP is not None:
             # situation load from folder
             __path = self.getPathImage()
@@ -313,7 +363,7 @@ class LBP(BaseImage):
                 result.append(temp)
             return np.asarray(result)
         
-        
+    # giống với hàm get_image_color_from_id ở trên nhưng được đọc ở dạng lbp
     def get_image_lbp_from_id(self, ids: list):
         """_summary_
 
@@ -351,6 +401,9 @@ class LBP(BaseImage):
             return np.asarray(result)
 
 class Distance:
+    """
+        Hàm tính khoảng cách sẽ được dùng từ [version 2.0] trở đi
+    """
     def __init__(self, object_one, 
                        object_two, 
                        database,
